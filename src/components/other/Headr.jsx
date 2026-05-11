12a2552c12e3afab3888ff24c6c71da337d04dc0
Naman-gautam <namangautam935@gmail.com>
2026-05-11 18:58:27 +0530

    change

--- src/components/other/Headr.jsx
+++ src/components/other/Headr.jsx
@@ -8,7 +8,7 @@ const Headr = ({data}) => {
   }
   return (
     <div className='flex justify-between items-end'>
-        <h1 className='text-2xl'>Hello <br />,<span className='text-3xl font-semibold'>{data.firstname}👋</span> </h1>
+        <h1 className='text-2xl'>Hello <br />,<span className='text-3xl font-semibold'>Name👋</span> </h1>
         <button onClick={logOutUser} className='bg-red-500 text-lg text-white rounded-md px-3 py-3'>Log Out</button>
     </div>
   )
